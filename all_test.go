7a1a29d0e8ba516651e108ea47b9f041484ff969
wwenhui <wwenhui@weforpay.com>
2017-10-10 12:17:27 +0800

    1.优化测试代码

--- all_test.go
+++ all_test.go
@@ -3,6 +3,7 @@ package mqweb
 import (
 	"fmt"
 	"net/http"
+	"sync"
 	"testing"
 	"time"
 
@@ -33,78 +34,72 @@ func BenchmarkHttp(b *testing.B) {
 	}
 
 }
+
+var once sync.Once
+
 func TestServer(t *testing.T) {
 	gateway := "localhost:1883"
-	s := NewServer(utils.NewId())
-	err := s.DialGeteWay(gateway)
+
+	once.Do(func() {
+		s := NewServer(utils.NewId())
+		err := s.DialGeteWay(gateway)
+		if err != nil {
+			fmt.Println(err.Error())
+			t.FailNow()
+			return
+		}
+		err = s.Serv("/api/v1/helloworld", func(id string, params []byte) {
+			s.Callback(id, []byte("helloworld"))
+		})
+		if err != nil {
+			fmt.Println(err.Error())
+			t.FailNow()
+			return
+		}
+	})
+
+	var b time.Time
+	c := NewClient()
+	err := c.DialGateWay(gateway)
 	if err != nil {
 		fmt.Println(err.Error())
 		t.FailNow()
 		return
 	}
+	defer c.Close()
+	b = time.Now()
+	result, err := c.Call("/api/v1/helloworld", []byte("helloworld"))
+	usetime := time.Since(b).String()
+	t.Logf("usetime:%s", usetime)
+	if err != nil {
 
-	go func() {
-		var b time.Time
-		err := s.Serv("/api/v1/helloworld", func(id string, params []byte) {
-			s.Callback(id, []byte("helloworld"))
-		})
-		c := NewClient()
-		err = c.DialGateWay(gateway)
-		if err != nil {
-			fmt.Println(err.Error())
-			t.FailNow()
-			return
-		}
-		defer c.Close()
-		b = time.Now()
-		result, err := c.Call("/api/v1/helloworld", []byte("helloworld"))
-		usetime := time.Since(b).String()
-		t.Logf("usetime:%s", usetime)
-		if err != nil {
-
-			fmt.Println(err.Error())
-			t.FailNow()
-			return
-		}
-		if string(result) != "helloworld" {
-			t.FailNow()
-		}
-		s.Close()
-	}()
-	s.Listen()
+		fmt.Println(err.Error())
+		t.FailNow()
+		return
+	}
+	if string(result) != "helloworld" {
+		t.FailNow()
+	}
 }
 
 func BenchmarkServer(b *testing.B) {
 	b.StopTimer()
+	var err error
 	gateway := "localhost:1883"
-	s := NewServer(utils.NewId())
-	err := s.DialGeteWay(gateway)
+	c := NewClient()
+	err = c.DialGateWay(gateway)
 	if err != nil {
 		fmt.Println(err.Error())
 		return
 	}
-
-	go func() {
-		err := s.Serv("/api/v1/helloworld", func(id string, params []byte) {
-			s.Callback(id, []byte("helloworld"))
-		})
-		c := NewClient()
-		err = c.DialGateWay(gateway)
+	defer c.Close()
+	b.StartTimer()
+	for i := 1; i < b.N; i++ {
+		_, err := c.Call("/api/v1/helloworld", []byte("helloworld"))
 		if err != nil {
 			fmt.Println(err.Error())
 			return
 		}
-		defer c.Close()
-		b.StartTimer()
-		for i := 1; i < b.N; i++ {
-			_, err := c.Call("/api/v1/helloworld", []byte("helloworld"))
-			if err != nil {
-				fmt.Println(err.Error())
-				return
-			}
-		}
-		b.StopTimer()
-		s.Close()
-	}()
-	s.Listen()
+	}
+	b.StopTimer()
 }
